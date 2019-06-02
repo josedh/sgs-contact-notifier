a498d64c2391698145266652b547c4c2d82e4bd2
Jose De La O <jdelao1988@gmail.com>
2019-06-02 14:47:23 -0400

    updates to pkg structure

--- main.go
+++ main.go
@@ -10,29 +10,14 @@ import (
 	"strings"
 	"time"
 
+	"sgs-notifier/models"
+
 	"github.com/jmoiron/sqlx"
 	_ "github.com/lib/pq"
 	log "github.com/sirupsen/logrus"
 )
 
-// Contact defines as contact as saved in the postgres table for sgs.com
-type contact struct {
-	ID           string    `db:"id"`
-	Name         string    `db:"name"`
-	Email        string    `db:"email"`
-	Phone        string    `db:"phone"`
-	Message      string    `db:"message"`
-	CaptchaScore string    `db:"captcha_score"`
-	Acknowledged bool      `db:"acknowledged"`
-	CreatedOn    time.Time `db:"created_on"`
-	UpdatedOn    time.Time `db:"updated_on"`
-}
-
 var isDev bool
-
-func (c contact) String() string {
-	return fmt.Sprintf("Contact name: %s, email: %s, phone: %s", c.Name, c.Email, c.Phone)
-}
 
 func init() {
 	if _, exists := os.LookupEnv("DEV"); exists {
@@ -66,43 +51,41 @@ func main() {
 	}
 
 	// new contact loop
-	tickChan := time.Tick(3 * time.Hour)
-	for {
-		select {
-		case <-tickChan:
-			loc, err := time.LoadLocation("America/New_York")
-			if err != nil {
-				log.Errorf("Failed to load EST location for date data, please contact webmaster")
-			}
-			currTime := time.Now()
-			start := time.Date(currTime.Year(), currTime.Month(), currTime.Day(), 9, 00, 00, 00, loc)
-			end := time.Date(currTime.Year(), currTime.Month(), currTime.Day(), 15, 00, 00, 00, loc)
-			if currTime.Before(start) ||
-				currTime.After(end) ||
-				currTime.Weekday() == time.Saturday ||
-				currTime.Weekday() == time.Sunday {
-				// if outside work hours, don't message
-				log.Info("Outside of work hours, skipping notifications")
-				continue
-			}
-			log.Infof("Checking sgs.com contacts table at: %v", time.Now().String())
-			if err := checkContacts(dbx); err != nil {
-				// there was an error checking for new contacts, log and report
-				log.Errorf("Failed to check postgres for new contacts on sgs.com: %v", err)
-			}
+	//tickChan := time.Tick(3 * time.Hour)
+	tickChan := time.Tick(3 * time.Second)
+	for range tickChan {
+		loc, err := time.LoadLocation("America/New_York")
+		if err != nil {
+			log.Errorf("Failed to load EST location for date data, please contact webmaster")
+		}
+		currTime := time.Now()
+		start := time.Date(currTime.Year(), currTime.Month(), currTime.Day(), 9, 00, 00, 00, loc)
+		end := time.Date(currTime.Year(), currTime.Month(), currTime.Day(), 15, 00, 00, 00, loc)
+		if currTime.Before(start) ||
+			currTime.After(end) ||
+			//currTime.Weekday() == time.Saturday ||
+			currTime.Weekday() == time.Sunday {
+			// if outside work hours, don't message
+			log.Info("Outside of work hours, skipping notifications")
+			continue
+		}
+		log.Infof("Checking sgs.com contacts table at: %v", time.Now().String())
+		if err := checkContacts(dbx); err != nil {
+			// there was an error checking for new contacts, log and report
+			log.Errorf("Failed to check postgres for new contacts on sgs.com: %v", err)
 		}
 	}
 }
 
 func checkContacts(dbx *sqlx.DB) error {
-	var res []contact
+	var contacts []models.Contact
 	var q = `SELECT
 				id, name, email, phone, message, captcha_score, acknowledged, created_on, updated_on
 			FROM
 				contacts
 			WHERE
 				acknowledged = false`
-	if err := dbx.Select(&res, q); err != nil {
+	if err := dbx.Select(&contacts, q); err != nil {
 		log.Debug(err)
 		return err
 	}
@@ -111,11 +94,11 @@ func checkContacts(dbx *sqlx.DB) error {
 	if twilioSID == "" || twilioAuth == "" {
 		return fmt.Errorf("Invalid twilio credentials, please check those on the server env and try again")
 	}
-	for _, r := range res {
-		log.Infof("Contact %s is unacknowledged, notifying...", r.Name)
-		if err := sendToPOC(r, twilioSID, twilioAuth); err != nil {
+	for _, c := range contacts {
+		log.Infof("Contact %s is unacknowledged, notifying...", c.Name)
+		if err := sendToPOC(c, twilioSID, twilioAuth); err != nil {
 			// An error occurred sending contact info to sgs admins. Log it
-			log.Errorf("Failed to send contact %v to POC: %v", r.String(), err)
+			log.Errorf("Failed to send contact %v to POC: %v", c.String(), err)
 		}
 		time.Sleep(15 * time.Second) // Give it some time before sending next contact
 	}
@@ -123,7 +106,7 @@ func checkContacts(dbx *sqlx.DB) error {
 	return nil
 }
 
-func sendToPOC(c contact, sid, auth string) error {
+func sendToPOC(c models.Contact, sid, auth string) error {
 	var (
 		urlStr = "https://api.twilio.com/2010-04-01/Accounts/" + sid + "/Messages.json"
 		client = &http.Client{}
@@ -152,7 +135,7 @@ func sendToPOC(c contact, sid, auth string) error {
 	return err
 }
 
-func formatMessage(c contact) strings.Reader {
+func formatMessage(c models.Contact) strings.Reader {
 	var msgToPOC = "We are being contacted by '%s' with email: '%s' and phone number '%s'" +
 		"for the following reason: '%s'.\n" +
 		"Please acknowledged receipt of this contact by replying '%s' to this message."
--- models/contact.go
+++ models/contact.go
@@ -1,6 +1,10 @@
-package model
+package models
 
-// Contact defines a contact from seamlessgutterservices.com
+import (
+	"fmt"
+)
+
+// Contact defines as contact as saved in the postgres table for sgs.com
 type Contact struct {
 	ID           string  `db:"id"`
 	Name         string  `db:"name"`
@@ -11,4 +15,8 @@ type Contact struct {
 	Acknowledged bool    `db:"acknowledged"`
 	CreatedOn    int64   `db:"created_on"`
 	UpdatedOn    int64   `db:"updated_on"`
+}
+
+func (c Contact) String() string {
+	return fmt.Sprintf("Contact name: %s, email: %s, phone: %s", c.Name, c.Email, c.Phone)
 }
